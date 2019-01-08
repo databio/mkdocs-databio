5d6a94d04a257477b8f1f653243f8a7b79b30d7d
nsheff <nsheff@users.noreply.github.com>
2019-01-08 13:19:05 -0500

    Add theme

---
+++ README.md
@@ -0,0 +1,20 @@
+# MkDocs Databio Theme
+
+This is the [mkdocs](http://mkdocs.org) theme for the Sheffield lab.
+
+## Install
+
+Install it with:
+
+```{bash}
+pip install --user --upgrade https://github.com/databio/mkdocs-databio/zipball/master
+```
+
+## Use it
+
+Render your mkdocs site with it adding this to your `mkdocs.yml` file:
+
+```{yaml}
+theme: databio
+```
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+
+setup(
+    name="mkdocs-databio",
+    version=VERSION,
+    url='http://github.com/databio/mkdocs-databio/',
+    license='BSD',
+    description='Databio theme for MkDocs',
+    author='Nathan Sheffield',
+    author_email='nathan@code.databio.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['mkdocs>=1.0'],
+    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    entry_points={
+        'mkdocs.themes': [
+            'databio = mkdocs_databio',
+        ]
+    },
+    zip_safe=False
+)
