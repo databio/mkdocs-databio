d09913eda25093ea7a320e9ae29f93fa7043187d
nsheff <nsheff@users.noreply.github.com>
2019-01-09 17:21:43 -0500

    move template into package

--- mkdocs_plugin/plugin.py
+++ mkdocs_plugin/plugin.py
@@ -1,4 +1,5 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs.commands.build import build
 
 import mkdocs
 import os
@@ -19,15 +20,15 @@ timer = 0
 
 class AutoDocumenter(BasePlugin):
     """
-    Inject certain config variables into the markdown
+    Populate automatic documentation markdown
     """
     config_scheme = (
-        ('foo', mkdocs.config.config_options.Type(mkdocs.utils.string_types, default='a default value')),
+        ('auto', mkdocs.config.config_options.Type(mkdocs.utils.string_types, default=None)),
         ('bar', mkdocs.config.config_options.Type(int, default=0)),
         ('baz', mkdocs.config.config_options.Type(bool, default=True))
     )
 
-
+    
     def on_files(self, files, config):
 
         # Add non docs_dir files that are rendered versions to this.
@@ -35,12 +36,13 @@ class AutoDocumenter(BasePlugin):
         inpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_SOURCE"])
         outpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_BUILD"])
 
-
-
         for nb in glob.glob(outpath + "/*.md"):
             nb = os.path.relpath(nb, config["auto"]["JUPYTER_BUILD"])
-            print(nb, os.path.abspath(config["auto"]["JUPYTER_BUILD"]), config['site_dir'], config['use_directory_urls'])
+            # print(nb, os.path.abspath(config["auto"]["JUPYTER_BUILD"]), config['site_dir'], config['use_directory_urls'])
             files.append(mkdocs.structure.files.File(nb, os.path.abspath(config["auto"]["JUPYTER_BUILD"]), config['site_dir'], config['use_directory_urls']))
+            nbm = os.path.splitext(nb)[0]+'.ipynb'
+            # print(nbm, os.path.abspath(config["auto"]["JUPYTER_SOURCE"]), config['site_dir'], config['use_directory_urls'])
+            files.append(mkdocs.structure.files.File(nbm, os.path.abspath(config["auto"]["JUPYTER_SOURCE"]), config['site_dir'], config['use_directory_urls']))
 
         # for i in files:
         #     if os.path.dirname(i.src_path) != "jupyter":
@@ -52,18 +54,21 @@ class AutoDocumenter(BasePlugin):
 
         # Here, watch the ipynb files
         inpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_SOURCE"])
-        outpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_BUILD"])
 
         for nb in glob.glob(inpath + "/*.ipynb"):
-            print("Add to watch: ", nb)
-            server.watch(nb)
+            print("Add to watch: {}".format(nb))
+
+            def builder():
+                build(config, live_server=True)
+
+            server.watch(nb, builder)
 
         return server
 
 
     def on_pre_build(self, config):
         global timer
-        time.sleep(1)
+        # time.sleep(1)
         print("Running AutoDocumenter plugin")
         print(timer, time.time())
         if time.time() - timer < 3:
@@ -71,14 +76,15 @@ class AutoDocumenter(BasePlugin):
         else:
             timer = time.time()
 
+        template = os.path.join(os.path.dirname(__file__), "templates/jupyter_markdown.tpl")
         inpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_SOURCE"])
         outpath = os.path.join(os.path.dirname(config["config_file_path"]), config["auto"]["JUPYTER_BUILD"])
 
         for nb in glob.glob(inpath + "/*.ipynb"):
             cmd = "jupyter nbconvert --to markdown" + \
-            " --template=~/mymarkdown.tpl" + \
+            " --template={tpl}".format(tpl=template) + \
             " {notebook} --output-dir {od}".format(notebook=nb, od=outpath)
 
-            print(cmd)
+            # print(cmd)
             subprocess.call(cmd, shell=True)
 
---
+++ mkdocs_plugin/templates/jupyter_markdown.tpl
@@ -0,0 +1,19 @@
+{% extends 'markdown.tpl' %}
+
+{% block stream %}
+```.output
+{{ output.text }}
+```
+{% endblock stream %}
+
+
+{% block input %}
+```
+{%- if 'magics_language' in cell.metadata  -%}
+    {{ cell.metadata.magics_language }}
+{%- elif 'name' in nb.metadata.get('language_info', {}) -%}
+    {{ nb.metadata.language_info.name }}
+{%- endif %}
+{{ cell.source}}
+```
+{% endblock input %}
